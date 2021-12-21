34aae5dfcd7e45ea0bf073150d1b10e35b450ecb
Jannat <Jannathaleel@gmail.com>
2021-12-21 17:08:39 +0000

    final commit

--- express_app.js
+++ express_app.js
@@ -10,6 +10,14 @@ const mongo_db_url = ''
 
 // const mongo_db_url = 'mongodb://localhost/todos_db';
 server.use(express.json());
+server.get('/',function(req, res){
+    res.status(200).json( {success: true, message: 'WELCOME, this is Jannats todo node App'})
+});
+server.get('/todos',todoController.getAllTodos); 
+server.get('/todo/:id',todoController.getTodoById); 
+server.post('/todo',todoController.insertTodo);
+server.put('/todo', todoController.updateTodoById);
+server.delete('/todo/:id',todoController.deleteTodoById);
 
 
 server.listen(PORT, function(){  
@@ -17,14 +25,6 @@ server.listen(PORT, function(){
     mongoose.connect(process.env.MONGO_DB_ATLAS_URL)
     .then (function(){
         console.log('DB is connected');
-        server.get('/',function(req, res){
-            res.status(200).json( {success: true, message: 'WELCOME, this is Jannats todo node App'})
-        });
-        server.get('/todos',todoController.getAllTodos); 
-        server.get('/todo/:id',todoController.getTodoById); 
-        server.post('/todo',todoController.insertTodo);
-        server.put('/todo', todoController.updateTodoById);
-        server.delete('/todo/:id',todoController.deleteTodoById);
     })
     .catch (function(error){
         console.log('DB is not connected:',error.message);
